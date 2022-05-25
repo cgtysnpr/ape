70f3faa8f007b35612d77bc05a07968d152f4abf
cgtysnpr0 <cagatay.sinpari@bamep.com>
2022-05-25 21:49:17 +0300

    discord

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ import AOS from "aos";
 import { useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init();
